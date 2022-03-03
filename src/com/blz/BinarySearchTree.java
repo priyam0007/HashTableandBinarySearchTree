0ab130d47c50a9055e6097928478dd57afeb24f4
Priya <Priyamadiwalar0007@gmail.com>
2022-03-03 22:55:21 +0530

    Added:Create the binary tree

--- src/com/blz/BinarySearchTree.java
+++ src/com/blz/BinarySearchTree.java
@@ -11,11 +11,11 @@ class Node {
 	public Node(int data) {
 		this.data = data;
 		this.left = this.right = null;
-
 	}
 }
 
 public class BinarySearchTree {
+
 	Node root;
 
 	BinarySearchTree(int key) {
@@ -46,6 +46,20 @@ public class BinarySearchTree {
 		tree.root = new Node(56);
 		tree.root.left = new Node(30);
 		tree.root.right = new Node(70);
+
+		// Adding elements in left and right of 30 and further
+		tree.root.left.left = new Node(22);
+		tree.root.left.right = new Node(40);
+		tree.root.left.left.left = new Node(11);
+		tree.root.left.left.left.left = new Node(3);
+		tree.root.left.left.left.right = new Node(3);
+
+		// Adding elements in left and right of 70 and further
+		tree.root.right.left = new Node(60);
+		tree.root.right.right = new Node(95);
+		tree.root.right.left = new Node(65);
+		tree.root.right.left = new Node(63);
+		tree.root.right.left = new Node(67);
 
 		tree.inorderTraversal(tree.root);
 
