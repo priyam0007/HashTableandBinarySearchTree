9c3216ee677dd0a6b5a2b68a18e355ab082658ae
Priya <Priyamadiwalar0007@gmail.com>
2022-03-09 22:34:59 +0530

    Added: Method to search element in Binary Tree

--- src/com/blz/BinarySearchTree.java
+++ src/com/blz/BinarySearchTree.java
@@ -11,6 +11,7 @@ class Node {
 	public Node(int data) {
 		this.data = data;
 		this.left = this.right = null;
+
 	}
 }
 
@@ -38,11 +39,33 @@ public class BinarySearchTree {
 		}
 	}
 
+	public static boolean flag = false;
+
+	public void searchNode(Node temp, int value) {
+		// Check whether tree is empty
+		if (root == null) {
+			System.out.println("Tree is empty");
+		} else {
+			// If value is found in the given binary tree then, set the flag to true
+			if (temp.data == value) {
+				flag = true;
+				return;
+			}
+			// Search in left subtree
+			if (flag == false && temp.left != null) {
+				searchNode(temp.left, value);
+			}
+			// Search in right subtree
+			if (flag == false && temp.right != null) {
+				searchNode(temp.right, value);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		BinarySearchTree tree = new BinarySearchTree();
-
 		tree.root = new Node(56);
 		tree.root.left = new Node(30);
 		tree.root.right = new Node(70);
@@ -52,7 +75,7 @@ public class BinarySearchTree {
 		tree.root.left.right = new Node(40);
 		tree.root.left.left.left = new Node(11);
 		tree.root.left.left.left.left = new Node(3);
-		tree.root.left.left.left.right = new Node(3);
+		tree.root.left.left.left.right = new Node(16);
 
 		// Adding elements in left and right of 70 and further
 		tree.root.right.left = new Node(60);
@@ -61,7 +84,12 @@ public class BinarySearchTree {
 		tree.root.right.left = new Node(63);
 		tree.root.right.left = new Node(67);
 
+		System.out.println("Level order traversal of binary tree is ");
+
 		tree.inorderTraversal(tree.root);
+		System.out.println();
+		tree.searchNode(tree.root, 60);
+		System.out.println("\nElement SEARCHED is exits: " + flag);
 
 	}
 }
